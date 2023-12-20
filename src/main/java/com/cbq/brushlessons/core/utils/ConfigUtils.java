a98ac18eca2557fb758a21d45650ec877c8ce04d
Changbaiqi <2084069833@qq.com>
2023-12-20 16:54:55 +0800

    添加配置文件错误提示

--- src/main/java/com/cbq/brushlessons/core/utils/ConfigUtils.java
+++ src/main/java/com/cbq/brushlessons/core/utils/ConfigUtils.java
@@ -4,18 +4,21 @@ import com.cbq.brushlessons.core.entity.Config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class ConfigUtils {
     public static Config loadingConfig(){
 
 
         File file = new File("./config.json");
         if(!file.exists()){
-            System.out.println("配置文件不存在!!!");
+//            System.out.println("配置文件不存在!!!");
+            log.error("亚托莉：你把配置文件丢哪去了，我根本读不到！（╯#-皿-)╯~~╧═╧");
             return null;
         }
 
@@ -27,9 +30,7 @@ public class ConfigUtils {
             Config config = objectMapper.readValue(configText, Config.class);
             return config;
         } catch (JsonProcessingException e) {
-            System.out.println("ATRI：json配置文件写错啦，（╯#-皿-)╯~~╧═╧");
-            System.out.println();
-
+            log.error("亚托莉：json配置文件写错啦!（╯#-皿-)╯~~╧═╧");
             throw new RuntimeException(e);
         }
 
