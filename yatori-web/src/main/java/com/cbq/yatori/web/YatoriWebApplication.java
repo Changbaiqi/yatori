ae21ecba81bd229dfb8f52767b44954f5b9234d2
Changbaiqi <2084069833@qq.com>
2024-01-01 17:21:19 +0800

    准备启动可视化Web项目计划

---
+++ yatori-web/src/main/java/com/cbq/yatori/web/YatoriWebApplication.java
@@ -0,0 +1,13 @@
+package com.cbq.yatori.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatoriWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YatoriWebApplication.class, args);
+    }
+
+}
---
+++ yatori-web/src/test/java/com/cbq/yatori/web/YatoriWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbq.yatori.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatoriWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
