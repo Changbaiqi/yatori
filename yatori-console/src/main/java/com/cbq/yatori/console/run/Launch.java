ae6f02f8f032d96262f3d90cae4280d415174789
Changbaiqi <changbaiqi@foxmail.com>
2024-05-23 14:39:43 +0800

    Modified version identification.

--- yatori-console/src/main/java/com/cbq/yatori/console/run/Launch.java
+++ yatori-console/src/main/java/com/cbq/yatori/console/run/Launch.java
@@ -50,7 +50,7 @@ public class Launch {
                       :  \\  \\;  :   .'   \\ |  ,   /  `----'   ---'    ;  :    ;\s
                        \\  ' ;|  ,     .-./  ---`-'                    |  ,   / \s
                         `--`  `--`---'                                 ---`-'  \s
-                                Yatori v2.0.1-Beta.4
+                                Yatori v2.0.1-Beta.5
                          仅用于学习交流，请勿用于违法和商业用途！！！
                   GitHub开源地址：https://github.com/Changbaiqi/yatori
                   个人博客：https://blogs.changbaiqi.top
