1cb3eea80885896452b731b72eda9e1144f23c80
Changbaiqi <changbaiqi@foxmail.com>
2024-12-04 21:18:54 +0800

    update: udpate xutxit

--- logic/xuexitong/XueXiTongPart.go
+++ logic/xuexitong/XueXiTongPart.go
@@ -4,12 +4,12 @@ import (
 	"log"
 	"yatori-go-console/config"
 
-	"github.com/yatori-dev/yatori-go-core/aggregation/yinghua"
-	yinghuaApi "github.com/yatori-dev/yatori-go-core/api/yinghua"
+	xuexitong "github.com/yatori-dev/yatori-go-core/aggregation/xuexitong"
+	xuexitongApi "github.com/yatori-dev/yatori-go-core/api/xuexitong"
 	lg "github.com/yatori-dev/yatori-go-core/utils/log"
 )
 
-// 用于过滤英华账号
+// 用于过滤学习通账号
 func FilterAccount(configData *config.JSONDataForConfig) []config.Users {
 	var users []config.Users //用于收集英华账号
 	for _, user := range configData.Users {
@@ -21,28 +21,24 @@ func FilterAccount(configData *config.JSONDataForConfig) []config.Users {
 }
 
 // 开始刷课模块
-func RunBrushOperation(setting config.Setting, users []config.Users, userCaches []*yinghuaApi.YingHuaUserCache) {
+func RunBrushOperation(setting config.Setting, users []config.Users, userCaches []*xuexitongApi.XueXiTUserCache) {
 
 }
 
 // 用户登录模块
-func UserLoginOperation(users []config.Users) []*yinghuaApi.YingHuaUserCache {
-	var UserCaches []*yinghuaApi.YingHuaUserCache
+func UserLoginOperation(users []config.Users) []*xuexitongApi.XueXiTUserCache {
+	var UserCaches []*xuexitongApi.XueXiTUserCache
 	for _, user := range users {
 		if user.AccountType == "XUEXITONG" {
-			cache := &yinghuaApi.YingHuaUserCache{PreUrl: user.URL, Account: user.Account, Password: user.Password}
-			error := yinghua.YingHuaLoginAction(cache) // 登录
+			cache := &xuexitongApi.XueXiTUserCache{Name: user.Account, Password: user.Password}
+			error := xuexitong.XueXiTLoginAction(cache) // 登录
 			if error != nil {
-				lg.Print(lg.INFO, "[", lg.Green, cache.Account, lg.White, "] ", lg.Red, error.Error())
+				lg.Print(lg.INFO, "[", lg.Green, cache.Name, lg.White, "] ", lg.Red, error.Error())
 				log.Fatal(error) //登录失败则直接退出
 			}
-			go keepAliveLogin(cache) //携程保活
+			// go keepAliveLogin(cache) //携程保活
 			UserCaches = append(UserCaches, cache)
 		}
 	}
 	return UserCaches
-}
-
-func keepAliveLogin(cache *yinghuaApi.YingHuaUserCache) {
-
 }
